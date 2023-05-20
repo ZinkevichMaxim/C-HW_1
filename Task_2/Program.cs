f20334fbf8e133ba7f8f139fee53cfe08595dae7
ZINKEVICH Maxim <drmvz@mail.ru>
2023-05-20 20:18:01 +0300

    fin_task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -2,3 +2,13 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = int.Parse(Console.ReadLine());
+if (A > B)
+{Console.WriteLine("Первое число больше второго");}
+else
+{Console.WriteLine("Второе число больше первого");}
+
