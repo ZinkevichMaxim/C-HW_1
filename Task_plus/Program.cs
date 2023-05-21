730a9666caae4c67054b5a8d6e42158c219d43c4
ZINKEVICH Maxim <drmvz@mail.ru>
2023-05-21 17:06:19 +0300

    positive numbers

---
+++ Task_plus/Program.cs
@@ -0,0 +1,9 @@
+﻿// task5 - (семинар1) - Напишите программу, которая на вход принимает одно число (N),
+//  а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int z = x * -1;
+do {Console.WriteLine(z);
+    z = z + 1;}
+    while (z <= x);
